d38836244b286574fcd0c6432e8b34abbeffee00
Varun Kamath <mail.varunkamath@gmail.com>
2023-11-09 20:43:48 +0530

    Backend ready login page

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,11 +1,15 @@
+"use client";
 import React from 'react';
 import Link from "next/link";
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export default function Navbar() {
-return (
-	<nav className="flex items-center justify-between
-	fixed left-0 right -0 px-8 h-12 bg-neutral-900 text-theme1">
-		<Link className = "font-bold" href={"/"}>SendIt</Link>
-	</nav>
-)
+	const {data} = useSession();
+	return (
+		<nav className="flex items-center justify-between
+		fixed left-0 right -0 px-8 h-12 bg-neutral-900 text-theme1">
+			<Link className = "font-bold" href={"/"}>SendIt</Link>
+			<button className="bg-neutral-800 p-2 rounded-sm" onClick={() => signOut('github')}>Sign Out of GitHub</button>
+		</nav>
+	)
 }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,85 +4,24 @@
 "use client";
 
 import { signIn, useSession, signOut } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import React from 'react';
-import { FC } from 'react';
 import "../globals.css";
 import { sign } from 'crypto';
 import { useRouter } from 'next/navigation';
-
-const CreateAccountForm = () => {
-  // const submitregistration = () => {
-  //   document.getElementById(<Login/>).style.display = 'block';
-  //   document.getElementById('register').style.display = 'none';
-  // };
-
-
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <form id="create-account-form" className="hideform" style={{ width: '30%' }}>
-      <br/>
-      <br/>
-      <h2>Create Account</h2>
-
-      {/* SRN input */}
-      <div className="input-group">
-        <label htmlFor="srn">SRN</label>
-        <input type="text" id="srn" name="srn" placeholder="Enter your SRN"/>
-      </div>
-
-      {/* First Name input */}
-      <div className="input-group">
-        <label htmlFor="firstName">First Name</label>
-        <input type="text" id="firstName" name="firstName" placeholder="Enter your first name" />
-      </div>
-
-      {/* Last Name input */}
-      <div className="input-group">
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName" placeholder="Enter your last name" />
-      </div>
-
-      {/* Email input */}
-      <div className="input-group">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email" />
-      </div>
-
-      {/* Password input */}
-      <div className="input-group">
-        <label htmlFor="password">Password</label>
-        <input type="password" id="newpassword" name="password" placeholder="Enter your password" />
-      </div>
-
-      {/* Section input */}
-      <div className="input-group">
-        <label htmlFor="section">Section</label>
-        <input type="text" id="section" name="section" placeholder="Enter your section" />
-      </div>
-
-      {/* Semester input */}
-      <div className="input-group">
-        <label htmlFor="semester">Semester</label>
-        <input type="text" id="semester" name="semester" placeholder="Enter your semester" />
-      </div>
-
-      {/* Submit button */}
-      <button className = "register" type="submit" /*onClick={submitregistration}*/>Create Account</button>
-    </form>
-    </div>
-  );
-};
-
 
 const Login = ()=>{
 
   const {data} = useSession();
   const {push} = useRouter();
 
-  const handleCreateAccountClick = () => {
-    document.getElementById('create-account-form').style.display = 'block';
-    document.getElementById('login-container').style.display = 'none';
-  };
+  const [srn, setSrn] = useState();
+  const [first, setFirst] = useState();
+  const [last, setLast] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [section, setSection] = useState();
+  const [semester, setSemester] = useState();
 
   if(data == null || data == undefined){
     return (
@@ -112,24 +51,74 @@ const Login = ()=>{
             <div className="flex flex-row gap-2">
               <button className="bg-neutral-800 p-2 rounded-sm" onClick={() => signIn("github")}>Sign in with GitHub</button>
               <button className="bg-neutral-800 p-2 rounded-sm" onClick={() => signIn("google")}>Sign in with Google</button>
-            </div>
-            <br/>
-            {/* Create Account link */}
-            <div className="create-account" style={{ display: 'flex', alignItems: "center" }}>
-              <h3 style={{ marginRight: '10px', alignContent: "center"}}>Not Registered?</h3>
-              <button onClick={handleCreateAccountClick} className='text-blue-400'>Create Account</button>
+              <button className="bg-neutral-800 p-2 rounded-sm" onClick={() => signOut("github")}>Sign Out</button>
             </div>
           </div>
         </div>
         {/* Display the CreateAccountForm by default */}
-        <CreateAccountForm />  
+        {/* <CreateAccountForm />   */}
       </section>
     );
   }else if (data != null || data != undefined){
-    push('/classrooms');
+    const userInDb = true // Get user from sql
+    if(userInDb){
+      push('/classrooms');
+    }else{
+      return (
+        <div className='flex justify-center items-center min-h-screen flex-col gap-5 '>
+          <div className='bg-gray-800 p-10 w-1/4 flex flex-col justify-between items-center gap-10'>
+            <h2>To continue please fill this form</h2>
+            <div className='flex flex-col gap-5 w-full'>
+              {/* SRN input */}
+              <div className='flex flex-col'>
+                <label htmlFor="srn">SRN</label>
+                <input onChange={(event)=>{setSrn(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="text" id="srn" name="srn" placeholder="Enter your SRN"/>
+              </div>
+        
+              {/* First Name input */}
+              <div className='flex flex-col'>
+                <label htmlFor="firstName">First Name</label>
+                <input onChange={(event)=>{setFirst(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="text" id="firstName" name="firstName" placeholder="Enter your first name" />
+              </div>
+        
+              {/* Last Name input */}
+              <div className='flex flex-col'>
+                <label htmlFor="lastName">Last Name</label>
+                <input onChange={(event)=>{setLast(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="text" id="lastName" name="lastName" placeholder="Enter your last name" />
+              </div>
+        
+              {/* Email input */}
+              <div className='flex flex-col'>
+                <label htmlFor="email">Email</label>
+                <input onChange={(event)=>{setEmail(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="email" id="email" name="email" placeholder="Enter your email" />
+              </div>
+        
+              {/* Password input */}
+              <div className='flex flex-col'>
+                <label htmlFor="password">Password</label>
+                <input onChange={(event)=>{setPassword(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="password" id="newpassword" name="password" placeholder="Enter your password" />
+              </div>
+        
+              {/* Section input */}
+              <div className='flex flex-col'>
+                <label htmlFor="section">Section</label>
+                <input onChange={(event)=>{setSection(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="text" id="section" name="section" placeholder="Enter your section" />
+              </div>
+        
+              {/* Semester input */}
+              <div className='flex flex-col'>
+                <label htmlFor="semester">Semester</label>
+                <input onChange={(event)=>{setSemester(event.target.value)}} className='text-black outline-none h-10 rounded-sm p-2' type="text" id="semester" name="semester" placeholder="Enter your semester" />
+              </div>
+        
+              {/* Submit button */}
+              <button className='bg-yellow-400 text-black rounded-sm h-10' type="submit" /*onClick={submitregistration}*/>Create Account</button>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
-  
 };
 
-// export const __client = true;
 export default Login;
